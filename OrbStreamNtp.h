e6907f904579f4445707470a2ee6304ba91660e8
zhenyuanzi <zhenyuanzi@oradar.com.cn>
2020-10-27 14:38:59 +0800

    only one recive and send

---
+++ OrbStreamNtp.h
@@ -0,0 +1,19 @@
+#ifndef ORB_STREAM_NTP_H
+#define ORB_STREAM_NTP_H
+typedef struct
+{
+	//unsigned char *pframe;
+	unsigned char ntp_status;
+	unsigned char frame_data[640*480*2];
+	char md5_str[16*2+1];
+	
+}ROB_STREAM_NTP;
+
+enum COMM_STATUS
+{
+	COMM_START	= 0,
+	COMM_CRC_ERROR,
+	COMM_RESET,
+} comm_status;
+
+#endif
--- server.c
+++ server.c
@@ -5,6 +5,7 @@
 #include <unistd.h>
 #include <assert.h>
 #include "md5.h"
+#include "OrbStreamNtp.h"
 
 #define __DEBUG
 #ifdef __DEBUG
@@ -33,23 +34,10 @@ int main (void)
             return -1;
     }
     while (1) {
-#if 0
-        if(feof(fp) == 1){
-                printf ("fread to the end of %s \n",filename);
-                fseek(fp, 0, SEEK_SET);
-                fread(buffer, 1, 640*480*2, fp); 
-                frame_count = 1; 
-                printf ("frame_count: %d \n",frame_count);
-        }else{
-                fread(buffer, 1, 640*480*2, fp);
-                frame_count++;
-                printf ("frame_count: %d \n",frame_count);
-                printf ("re fread \n");
-        } 
-#else
+
         if (fread(buffer, 1, 640*480*2, fp) != 640*480*2){
                 // Loop
-                DEBUG("re fread!!!\n");
+                DEBUG("get to file end!!!\n");
                 fseek(fp, 0, SEEK_SET);
                 fread(buffer, 1, 640*480*2, fp);
                 frame_count = 1;
@@ -57,8 +45,6 @@ int main (void)
                 frame_count++;
         }
         DEBUG ("frame_count: %d \n",frame_count);
-#endif
-
 
         char rec_buffer [10];
         zmq_recv (responder, rec_buffer, 10, 0);
@@ -75,3 +61,45 @@ int main (void)
     }
     return 0;
 }
+
+#if 0 
+void * orb_ntp_init(char*server)
+{
+        void *context = zmq_ctx_new ();
+        void *responder = zmq_socket (context, ZMQ_REP);
+        int rc = zmq_bind (responder, "tcp://*:5555");
+        assert (rc == 0);
+        return responder;
+}
+orb_ntp_worker(void* resp , unsigned char *pdata unsigned int sizes)
+{
+        char rec_buffer [5],send_buffer[5];
+        ROB_STREAM_NTP rframe;
+        ROB_STREAM_NTP * pframe;
+        pframe->frame_data = pdata;
+        unsigned char comm_status;
+
+        zmq_recv (resp, rec_buffer, 5, 0);
+        DEBUG ("Received %s \n",rec_buffer);
+        if(strcmp(rec_buffer,"Start") == 0){
+                comm_status = COMM_START;   
+        }else{
+                comm_status = COMM_RESET;  
+         }
+       pframe->ntp_status = comm_status;
+        switch(comm_status)
+        {
+                case COMM_START:
+                        Compute_string_md5(pframe->frame_data, 640*480*2, pframe->md5_str);
+                        zmq_send (resp, (unsigned char*)pframe, sizeof(rframe), 0);
+                        DEBUG ("Send raw data \n");  
+                        break;
+                case COMM_CRC_ERROR:
+                        break;
+                case COMM_RESET:
+                        send_buffer="RESET"
+                        zmq_send (resp,send_buffer,5,0);
+                        break;
+        }  
+}
+#endif
